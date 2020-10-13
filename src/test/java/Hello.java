1a0e67980e9ef992e36b26525b8d6ccd9b77bd41
Joomart7878 <65704743+Joomart7878@users.noreply.github.com>
2020-10-13 23:42:50 +0600

    conflict
    This will conflict

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("My coworker added Really awesome Hello World"); // We are simulating my co-worker made this change
+        System.out.println("My coworker added Really awesome Hello World"); // We are simulating the situation my co-worker made this change!!!
         
         
     }
