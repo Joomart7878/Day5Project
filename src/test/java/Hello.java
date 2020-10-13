63a85e9c2e89bdd123df5aa5a176885d74c8a790
Joomart7878 <65704743+Joomart7878@users.noreply.github.com>
2020-10-13 23:35:42 +0600

    changes ito same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Really awesome Hello World");
+        System.out.println("Really awesome Hello World"); //changes same line
     }
 }
