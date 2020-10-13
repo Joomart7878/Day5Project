dc8f1da64b2dad55dfb0252260568f67edb66113
Joomart7878 <65704743+Joomart7878@users.noreply.github.com>
2020-10-13 23:46:01 +0600

    trying again to understand

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Really awesome Hello World"); //changes same line
+        System.out.println("Can not understand what is going on");
     }
 }
