2f5d730c96d3d1024964ffac2b1dd567ab1d6e3e
Joomart7878 <65704743+Joomart7878@users.noreply.github.com>
2020-10-13 23:34:56 +0600

    Update Hello.java

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Really awesome Hello World");
+        
+        System.out.println("FROM GITHUB MADE SOME CHANGES");
     }
 }
