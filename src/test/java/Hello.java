5c090619f492ca5b063711257d6368b2cb70ac5c
Joomart7878 <65704743+Joomart7878@users.noreply.github.com>
2020-10-13 23:47:31 +0600

    hellllo
    some changes here

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Really awesome Hello World"); //changes same line
+        //co-worker pushed some new changes here that i did not know till now
+        //and im trying to understand
     }
 }
